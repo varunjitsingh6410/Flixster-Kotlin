52bf5a9e083f0c77feed560afb3b05bfc1991da6
Varun Jit Singh <varunjitsingh@Varuns-MacBook-Pro.local>
2021-12-24 12:29:26 -0800

    implement recycler view

---
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.flixster
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+private const val TAG = "MovieAdapter"
+
+class MovieAdapter(private val context: Context, private val movies: List<Movie>)
+    : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+    inner class ViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
+        private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+        private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
+        fun bind(movie: Movie) {
+            tvTitle.text = movie.title
+            tvOverview.text = movie.overview
+
+            Glide.with(context).load(movie.posterImageUrl).into(ivPoster)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        Log.i(TAG, "onCreateViewHolder")
+        val view = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.i(TAG, "onBindViewHolder position $position")
+        val movie = movies[position]
+        holder.bind(movie)
+    }
+
+    override fun getItemCount() = movies.size
+}
